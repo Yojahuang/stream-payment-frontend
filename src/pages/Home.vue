8e944b8512980bb20090a38d141a011fa57b6e25
You-Jia Huang <yojahuang30613@gmail.com>
2023-05-07 19:27:19 +0800

    feat: update landing page and website title

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,9 +1,9 @@
 <template>
     <div class="w-75 mx-auto">
-        <div class="text-center font-weight-bold text-h2 mb-4">Stream Payment</div>
+        <div class="text-center font-weight-medium text-h3 mb-4">Stream Payment</div>
         <div class="text-center font-weight-medium">Real-time payments made easy with Stream Payment and blockchain.
         </div>
-        <div class="text-center text-h3 mb-4">About us</div>
+        <div class="text-center text-h3 my-4">About us</div>
         <div class="d-flex w-full align-center justify-center mx-auto">
             <Avatar class="mx-10" fullname="You-Jia Huang" filename="avatars/YJH-avatar.jpeg" nickname="Yojahuang"
                 jobTitle="Frontend Developer" githubLink="https://github.com/Yojahuang"></Avatar>
