3e7b9326f55182adbc80044e0a72d78167c1cdd2
You-Jia Huang <yojahuang30613@gmail.com>
2023-05-10 23:53:23 +0800

    feat: allow user to claim payment and add validate rules

--- src/pages/Dashboard/Record.vue
+++ src/pages/Dashboard/Record.vue
@@ -11,7 +11,7 @@
                     <v-progress-circular v-if="!smAndDown" :size="340" :width="0"
                         :color="Global.currentThemeIsDark() ? 'white' : 'black'">
                         You had withdraw: {{ record.withdraw }} DAI <br />
-                        You can still withdraw: {{ record.remainToken }} DAI
+                        You can still withdraw: {{ (calcClaimeableAmount - record.withdraw).toFixed(2) }} DAI
                     </v-progress-circular>
                 </v-progress-circular>
             </v-progress-circular>
@@ -19,10 +19,19 @@
 
         <div class="my-2" v-if="smAndDown">
             You had withdraw: <div class="amount-of-money">{{ record.withdraw }}</div> DAI <br />
-            You can still withdraw: <div class="amount-of-money">{{ calcClaimeableAmount - record.withdraw }}</div> DAI
+            You can still withdraw: <div class="amount-of-money">{{ (calcClaimeableAmount - record.withdraw).toFixed(2) }}
+            </div> DAI
         </div>
 
-        <v-btn color="primary">Claim payment</v-btn>
+        <div>
+            <v-btn prepend-icon="mdi-refresh">Refresh</v-btn>
+        </div>
+
+        <div :class="{ 'my-2': true, 'd-flex': !smAndDown, 'align-center': !smAndDown }">
+            <v-text-field v-model="claimAmount" label="How much you want to claim" :rules="checkClaimRule"
+                hide-details="auto"></v-text-field>
+            <v-btn @click="claimPayment()" color="primary">Claim payment</v-btn>
+        </div>
 
         <div class="text-h5 text-md-h4 text-lg-h4 my-2 font-weight-medium">
             Transcation history
@@ -58,14 +67,18 @@
 </template>
 
 <script setup lang="ts">
-import { useRecordStore } from '@/stores/Record'
 import { useRoute } from "vue-router"
 import { useDisplay } from 'vuetify'
 import { onMounted, ref, computed } from 'vue'
+import { storeToRefs } from "pinia"
+import { useRecordStore } from '@/stores/Record'
 import { useGlobalStore } from '@/stores/Global'
 import { Global } from "@/composables/Global"
+import StreamPaymentContract from '@/composables/StreamPayment'
 
 const globalStore = useGlobalStore()
+const { loadingSemaphore } = storeToRefs(globalStore)
+
 
 const { smAndDown } = useDisplay()
 
@@ -73,6 +86,14 @@ const recordStore = useRecordStore()
 const router = useRoute()
 const id = Number(router.params.id)
 const record = ref<any>({ "id": -1, "title": "-1", "startAt": new Date("2023-05-11T13:14:00.000Z"), "endAt": new Date("2023-05-17T13:14:00.000Z"), "remainToken": 0, "all": 0, "withdraw": 0, "identity": "Creator" })
+
+const currentTime = ref(Math.floor(new Date().getTime() / 1000))
+
+setInterval(() => {
+    currentTime.value = Math.floor(new Date().getTime() / 1000);
+}, 1000)
+
+const claimAmount = ref(0)
 
 interface Transcation {
     tx: string;
@@ -90,12 +111,11 @@ const beautifyAmount = (amount: number) => {
 }
 
 const calcClaimeableAmount = computed(() => {
+    const currentTimeRaw = currentTime.value
     const startTime = Math.floor(record.value.startAt.getTime() / 1000);
     const endTime = Math.floor(record.value.endAt.getTime() / 1000);
 
-    const currentTime = Math.floor(new Date().getTime() / 1000);
-
-    const result = record.value.all * (currentTime - startTime) / (endTime - startTime)
+    const result = Math.max(record.value.all * (currentTimeRaw - startTime) / (endTime - startTime), 0)
 
     return result
 })
@@ -118,6 +138,26 @@ onMounted(() => {
         })
     }
 })
+
+const checkClaimRule = [
+    (value: string) => {
+        const val = Number(value)
+        if (Number.isNaN(val)) return "Please input a positive integer number"
+        if (val <= 0) return "Please input a positive number"
+        if (val > calcClaimeableAmount.value) return "You can't withdraw more than you supposed to"
+        return true
+    }
+]
+
+const claimPayment = async () => {
+    loadingSemaphore.value += 1
+
+    const streamPaymentContract = new StreamPaymentContract()
+    streamPaymentContract.init()
+
+    await streamPaymentContract.claimPayment(BigInt(id), BigInt(claimAmount.value))
+    loadingSemaphore.value -= 1
+}
 </script>
 
 <style scoped>
